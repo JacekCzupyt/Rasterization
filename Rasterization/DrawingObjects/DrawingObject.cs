3bd56218a1aae9a02c9b17153aa89d6a7c5e56be
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-21 11:32:22 +0200

    Added IDrawingObject interface

---
+++ Rasterization/DrawingObjects/DrawingObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rasterization.DrawingObjects
+{
+    interface IDrawingObject
+    {
+        byte[] Draw(byte[] RgbValues, int stride, int width, int height);
+
+        Vector2 GetClosestPoint(Vector2 pos);
+        IEnumerable<Vector2> GetAllPoints();]
+        Color color { get; set; }
+    }
+}
