1ada829e51d79ab7f3911427f04dd2ec143ae9c8
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-22 17:07:49 +0200

    Added drawing buttons

---
+++ Rasterization/DrawingMouseControls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Rasterization
+{
+    public partial class MainWindow : Window
+    {
+
+    }
+}
--- Rasterization/MainWindow.xaml.cs
+++ Rasterization/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace Rasterization
         public MainWindow()
         {
             InitializeComponent();
-            //InitializeImage();
         }
+
     }
 }
