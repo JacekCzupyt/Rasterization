06d88bbbca7a74603887071009dcc320c402e900
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-23 17:19:54 +0200

    Moving Points first draft

--- Rasterization/DrawingMouseControls.cs
+++ Rasterization/DrawingMouseControls.cs
@@ -23,6 +23,7 @@ namespace Rasterization
         }
 
         UIState currentState = UIState.Nothing;
+        Vector2 PreviousMousePosition;
 
         private void MainImageContainer_MouseMove(object sender, MouseEventArgs e)
         {
@@ -32,6 +33,14 @@ namespace Rasterization
                     currentlyDrawnPoint.Point = e.GetPosition(MainImageContainer).ToVector2();
                     UpdateMainImage();
                     break;
+                case UIState.MovingExistingPoints:
+                    var newMousePos = e.GetPosition(MainImageContainer).ToVector2();
+                    foreach (var p in selectedPoints.Keys)
+                    {
+                        p.Point += newMousePos - PreviousMousePosition;
+                    }
+                    PreviousMousePosition = newMousePos;
+                    break;
             }
         }
         
@@ -39,6 +48,8 @@ namespace Rasterization
         {
             switch (currentState)
             {
+                case UIState.MovingExistingPoints:
+                case UIState.SelectingPoints:
                 case UIState.Nothing:
                     HandlePointSelection(e);
                     UpdateMainImage();
@@ -58,11 +69,27 @@ namespace Rasterization
             }
         }
 
+        private void MainImageContainer_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            switch (currentState)
+            {
+                case UIState.MovingExistingPoints:
+                    currentState = UIState.SelectingPoints;
+                    break;
+            }
+        }
+
 
         private void MainImageContainer_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
             switch (currentState)
             {
+                case UIState.MovingExistingPoints:
+                case UIState.SelectingPoints:
+                    selectedPoints.Clear();
+                    UpdateMainImage();
+                    currentState = UIState.Nothing;
+                    break;
                 case UIState.PreparingToDraw:
                     currentlyPressedButton.IsChecked = false;
                     currentlyPressedButton = null;
@@ -82,24 +109,76 @@ namespace Rasterization
 
             Vector2 pos = e.GetPosition(MainImageContainer).ToVector2();
 
-            IEnumerable<DrawingPoint> ClosestPoints = DrawingObjects.Select(obj => obj.GetClosestPoint(pos));
+            IEnumerable<(IDrawingObject, DrawingPoint)> ClosestPoints = DrawingObjects.Select(obj => (obj, obj.GetClosestPoint(pos)));
 
             if (ClosestPoints.Any())
             {
-                DrawingPoint ClosestPoint = ClosestPoints.Aggregate((min, x) => min.dist(pos) < x.dist(pos) ? min : x);
+                (IDrawingObject, DrawingPoint) ClosestPoint = ClosestPoints.Aggregate((min, x) => min.Item2.dist(pos) < x.Item2.dist(pos) ? min : x);
 
-                if (ClosestPoint.dist(pos) <= MaximumSelectDistance)
+                //Ctrl
+                if (Keyboard.IsKeyDown(Key.LeftCtrl))
                 {
-                    if (selectedPoints.ContainsKey(ClosestPoint))
+                    //Clicked point
+                    if (ClosestPoint.Item2.dist(pos) <= MaximumSelectDistance)
                     {
-                        selectedPoints.Remove(ClosestPoint);
+                        //Clicked once
+                        if (e.ClickCount == 1)
+                        {
+                            //Point already selected
+                            if (selectedPoints.ContainsKey(ClosestPoint.Item2))
+                            {
+                                //Deselect point
+                                selectedPoints.Remove(ClosestPoint.Item2);
+                            }
+                            //Point not selected
+                            else
+                            {
+                                //Select point
+                                selectedPoints.Add(ClosestPoint.Item2, new FilledCircle(ClosestPoint.Item2, PointRadius, System.Drawing.Color.Blue));
+                            }
+                        }
+                        //Clicked twice
+                        else
+                        {
+                            //Select shape
+                            foreach(DrawingPoint p in ClosestPoint.Item1.GetTranslationPoints())
+                                if(!selectedPoints.ContainsKey(p))
+                                    selectedPoints.Add(p, new FilledCircle(p, PointRadius, System.Drawing.Color.Blue));
+                        }
                     }
-                    else
+                }
+                //no Ctrl
+                else
+                {
+                    //Clicked point
+                    if (ClosestPoint.Item2.dist(pos) <= MaximumSelectDistance)
                     {
-                        selectedPoints.Add(ClosestPoint, new FilledCircle(ClosestPoint, PointRadius, System.Drawing.Color.Blue));
+                        //Clicked once
+                        if (e.ClickCount == 1)
+                        {
+                            //Point not selected
+                            if (!selectedPoints.ContainsKey(ClosestPoint.Item2))
+                            {
+                                //Select only this point
+                                selectedPoints.Clear();
+                                selectedPoints.Add(ClosestPoint.Item2, new FilledCircle(ClosestPoint.Item2, PointRadius, System.Drawing.Color.Blue));
+                            }
+                        }
+                        //Clicked twice
+                        else
+                        {
+                            //Select only this shape
+                            selectedPoints.Clear();
+                            foreach (DrawingPoint p in ClosestPoint.Item1.GetTranslationPoints())
+                                if (!selectedPoints.ContainsKey(p))
+                                    selectedPoints.Add(p, new FilledCircle(p, PointRadius, System.Drawing.Color.Blue));
+                        }
+                            
                     }
                 }
             }
+            PreviousMousePosition = e.GetPosition(MainImageContainer).ToVector2();
+            currentState = selectedPoints.Count > 0 ? UIState.MovingExistingPoints : UIState.Nothing;
         }
     }
 }
