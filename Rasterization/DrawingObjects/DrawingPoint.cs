52197ce8d503e0a84ed3d1dba0006b746c99e524
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-23 16:30:57 +0200

    Revert "Added NotifyPropertyChanged to DrawingPoint"
    This reverts commit 6b44c9a78aff76ff4bf055c40e56c4a65293801d.

--- Rasterization/DrawingObjects/DrawingPoint.cs
+++ Rasterization/DrawingObjects/DrawingPoint.cs
@@ -1,44 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rasterization.DrawingObjects
 {
-    class DrawingPoint : INotifyPropertyChanged
+    class DrawingPoint
     {
         public DrawingPoint(Vector2 p) { Point = p; }
         public static explicit operator DrawingPoint(Vector2 p) { return new DrawingPoint(p); }
 
-        private Vector2 point;
-        public Vector2 Point
-        {
-            get
-            {
-                return point;
-            }
-            set
-            {
-                point = value;
-                OnPropertyChanged();
-            }
-        }
+        public Vector2 Point;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public float X { get { return Point.X; }}
-        public float Y { get { return Point.Y; }}
+        public float X { get { return Point.X; } set { Point.X = value; } }
+        public float Y { get { return Point.Y; } set { Point.Y = value; } }
 
         public float dist(Vector2 pos) { return (pos - Point).Length(); }
-
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
     }
 }
