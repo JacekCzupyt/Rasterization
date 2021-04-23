3004b1d5886bda3a197d8f91d37feedbd2a0bb14
JacekCzupyt <kuba.czupyt@gmail.com>
2021-04-23 17:35:59 +0200

    Re-added NotifyPropertyChanged

--- Rasterization/DrawingObjects/DrawingPoint.cs
+++ Rasterization/DrawingObjects/DrawingPoint.cs
@@ -1,23 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Rasterization.DrawingObjects
 {
-    class DrawingPoint
+    class DrawingPoint : INotifyPropertyChanged
     {
         public DrawingPoint(Vector2 p) { Point = p; }
         public static explicit operator DrawingPoint(Vector2 p) { return new DrawingPoint(p); }
 
-        public Vector2 Point;
+        private Vector2 point;
+        public Vector2 Point
+        {
+            get
+            {
+                return point;
+            }
+            set
+            {
+                point = value;
+                OnPropertyChanged();
+            }
+        }
 
-        public float X { get { return Point.X; } set { Point.X = value; } }
-        public float Y { get { return Point.Y; } set { Point.Y = value; } }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public float X { get { return Point.X; }}
+        public float Y { get { return Point.Y; }}
 
         public float dist(Vector2 pos) { return (pos - Point).Length(); }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
